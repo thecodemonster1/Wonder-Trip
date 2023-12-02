6eb19e6046c5a1213e5c31e2242fdb18e9d25aed
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-12-02 11:08:41 +0530

    commit #2

--- app/src/main/java/com/example/wonder_trip_project/MainActivity.java
+++ app/src/main/java/com/example/wonder_trip_project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.wonder_trip_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Click handling method
+    public void onClickableTextClick(View view) {
+        // Create an Intent to navigate to the next activity
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
     }
 }
