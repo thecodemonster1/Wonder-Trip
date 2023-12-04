69a79eda968abfcaf9648985bf98b1eb7f85fd5d
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-12-04 16:27:12 +0530

    Pushing the users details to Firebase DB

--- app/src/main/java/com/example/wonder_trip_project/SignInActivity.java
+++ app/src/main/java/com/example/wonder_trip_project/SignInActivity.java
@@ -22,6 +22,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignInActivity extends AppCompatActivity {
     EditText txtUsername, txtEmail, txtPassword, txtPassword2, txtDOB;
@@ -35,7 +37,8 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        mAuth = FirebaseAuth.getInstance();
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users"); // Push the datas to "users" node
+        String userId = usersRef.push().getKey(); // Generates a unique key for the new user
 
         txtUsername = findViewById(R.id.txtRegUsername);
         txtEmail = findViewById(R.id.txtRegEmail);
@@ -46,47 +49,46 @@ public class SignInActivity extends AppCompatActivity {
         imgButton = findViewById(R.id.imgButton);
 //        progBar = findViewById(R.id.progressBar);
 
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnSignUpHandler();
-            }
-        });
-
-        imgButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-    }
-
-
-    public void btnSignUpHandler(){
+                String username = txtUsername.getText().toString();
+                String email = txtEmail.getText().toString();
+                String password = txtPassword.getText().toString();
+                String password2 = txtPassword2.getText().toString();
+                String dob = txtDOB.getText().toString();
 //        progBar.setVisibility(View.VISIBLE);
-        String username = txtUsername.getText().toString();
-        String email = txtEmail.getText().toString();
-        String password = txtPassword.getText().toString();
-        String password2 = txtPassword2.getText().toString();
-        String dob = txtDOB.getText().toString();
 
-        System.out.println("Usrname: "+username + "\nEmail: " + email + "\nPassword: " + password + "\nPassword2: " + password2 + "\nDate Of Birth: " + dob);
+                System.out.println("Usrname: "+username + "\nEmail: " + email + "\nPassword: " + password + "\nPassword2: " + password2 + "\nDate Of Birth: " + dob);
 
-        if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !password2.isEmpty() && !dob.isEmpty()){
-            if (password.equals(password2)){
-                Toast.makeText(SignInActivity.this, "Signing Up Successful", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-                intent.putExtra("regUsername", username);
-                intent.putExtra("regPassword", password);
-                startActivity(intent);
-            }else {
-                Toast.makeText(SignInActivity.this, "Passwords are doesn't match", Toast.LENGTH_SHORT).show();
+                if (!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !password2.isEmpty() && !dob.isEmpty()){
+                    if (password.equals(password2)){
+                        Toast.makeText(SignInActivity.this, "Signing Up Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+
+                        // Pushing the users data
+                        usersRef.child(userId).child("username").setValue(username);
+                        usersRef.child(userId).child("email").setValue(email);
+                        usersRef.child(userId).child("password").setValue(password);
+                        usersRef.child(userId).child("dob").setValue(dob);
+
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(SignInActivity.this, "Passwords are doesn't match", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(SignInActivity.this, "You missed on or more field to fill", Toast.LENGTH_SHORT).show();
+                }
+
             }
-        }else{
-            Toast.makeText(SignInActivity.this, "You missed on or more field to fill", Toast.LENGTH_SHORT).show();
-        }
+        });
 
+//        imgButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
     }
-
-
 }
