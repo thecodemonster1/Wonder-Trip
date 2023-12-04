459b2ee4d90d9e10fa5cedd785063f58eaed6a24
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-12-04 16:51:59 +0530

    Retriving the users details from Firebase DB

--- app/src/main/java/com/example/wonder_trip_project/MainActivity.java
+++ app/src/main/java/com/example/wonder_trip_project/MainActivity.java
@@ -19,6 +19,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     EditText txtUsername, txtPassword;
@@ -31,16 +36,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get a reference to the "users" node in the database
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
+
         mAuth = FirebaseAuth.getInstance();
 
         txtUsername = findViewById(R.id.txtUsername);
         txtPassword = findViewById(R.id.txtPassword);
         btnLogIn = findViewById(R.id.btnLogIn);
 
+
+
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btnLogInHandler();
+
+//        progBar.setVisibility(View.VISIBLE);
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                usersRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again whenever data at this location is updated.
+                        for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                            // Get individual user details
+                            String regUsername = userSnapshot.child("username").getValue(String.class);
+                            String regEmail = userSnapshot.child("email").getValue(String.class);
+                            String regPassword = userSnapshot.child("password").getValue(String.class);
+
+                            // Do something with the user details
+//                            Toast.makeText(MainActivity.this, "Username: " + regUsername + ", Email: " + regEmail + ", Password: " + regPassword, Toast.LENGTH_SHORT).show();
+                            if (username.equals(regUsername) && password.equals(regPassword)){
+                                if (username.isEmpty() || password.isEmpty()){
+                                    Toast.makeText(MainActivity.this, "Can't LogIn Field(s) are empty", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(MainActivity.this, "LogIn Successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+//                            if (!(username.equals(regUsername)) || !(password.equals(regPassword))){
+//                                Toast.makeText(MainActivity.this, "Email or Password Doesn't match", Toast.LENGTH_SHORT).show();
+//                            }123
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Failed to read value
+                        Log.w("Firebase", "Failed to read value.", databaseError.toException());
+                    }
+                });
             }
         });
     }
@@ -50,26 +97,5 @@ public class MainActivity extends AppCompatActivity {
         // Create an Intent to navigate to the next activity
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
-    }
-
-    public void btnLogInHandler() {
-//        progBar.setVisibility(View.VISIBLE);
-        String username = txtUsername.getText().toString();
-        String password = txtPassword.getText().toString();
-
-        Intent intent = getIntent();
-        String regUsername = intent.getStringExtra("regUsername");
-        String regPassword = intent.getStringExtra("regPassword");
-
-        if (username.equals(regUsername) && password.equals(regPassword)){
-            Toast.makeText(MainActivity.this, "LogIn Successful", Toast.LENGTH_SHORT).show();
-            intent = new Intent(getApplicationContext(), HomeActivity.class);
-            startActivity(intent);
-        }else if (username.isEmpty() || password.isEmpty()){
-            Toast.makeText(this, "Can't LogIn Field(s) are empty", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "Email or Password Doesn't match", Toast.LENGTH_SHORT).show();
-        }
-
     }
 }
